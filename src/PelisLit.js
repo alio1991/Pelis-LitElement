3c26505c7aea69d962bd481b0cd2e71c0869e526
alio1991 <alioli_15@hotmail.com>
2020-04-23 00:13:08 +0200

    main app refactor

--- src/PelisLit.js
+++ src/PelisLit.js
@@ -9,12 +9,32 @@ export class PelisLit extends LitElement {
       :host {
         height: 100%;
         width: 100%;
+        display: flex;
+        flex-direction: column;
+      }
+
+      body{
+        margin: 0;
+        padding: 0;
       }
 
       input-search{
-        height: 300px;
+        height: 110px;
         width: 100%;
-        background-color: red;
+        background-color: yellow;
+
+      }
+
+      .standard{
+        width: 70%;
+      }
+
+      .favorites{
+        width: 30%;
+      }
+
+      .content{
+        display: flex;
       }
     `;
   }
@@ -23,19 +43,50 @@ export class PelisLit extends LitElement {
     return {
 			movies:{
         type: Array
+      },
+      favorites:{
+        type: Array
       }
 		}
   }
 
   constructor(){
     super();
-    this.movies = [{Title:'EXAMPLE'}];
+    document.addEventListener("inputChanges", ev => { this.getMovies(ev.detail.value) });
+    document.addEventListener("cardSelected", ev => { this.cardSelected(ev.detail) });
+    this.movies = [];
+    this.favorites = this.loadFromLocalStorage('favorites') || [];
   }
 
 
-  getMovies({target}){
-    this.movies = [{Title:'1'},{Title:'2'}];
-    const {value} = target;
+  cardSelected({id, type}){
+    if(type === 'standard'){ //Seleccionada carta resultados
+      const movie = this.getMovieById(id, this.movies);
+      if(this.favorites.includes(movie)){ //La pelicula ya está en favoritos
+        return; //No hacemos nada
+      }else{//La pelicula no está en favoritos
+        this.favorites = [...this.favorites,movie]
+        this.saveToLocalStorage('favorites', this.favorites)
+      }
+    }else{ //Seleccionada carta de favoritos
+      this.favorites = this.favorites.filter(elem => elem.imdbID !==id);
+      this.saveToLocalStorage('favorites', this.favorites)
+    }
+  }
+  
+  getMovieById(id, list){
+    return list.find(elem => elem.imdbID === id)
+  }
+
+  saveToLocalStorage(name, item){ 
+    localStorage.setItem(name,  JSON.stringify(item));
+  }
+
+  loadFromLocalStorage(name){ 
+    return JSON.parse(localStorage.getItem(name));
+  }
+
+  getMovies(value){
     if(value.length >3){
       const page = 1;
       const type = 'movie';
@@ -44,22 +95,21 @@ export class PelisLit extends LitElement {
         return response.json();
       }).then(result => {
         if(result.Search && result.Search.length > 0){
-          // this.set('movies',result.Search);
           this.movies = Object.assign([], result.Search);
-          // this.movies = [...result.Search]; 
-          console.log(this.movies);
         }else{
-          this.movies = [];        
+          this.movies = Object.assign([], []);       
         }
       });
     }
-   
   }
 
   render() {
     return html`
-      <input-search .inputChanges=${this.getMovies} .title=${'Buscador de Películas'} ></input-search>
-      <movie-list .list=${this.movies}></movie-list>
+      <input-search .eventName=${'inputChanges'} .title=${'Buscador de Películas'} ></input-search>
+      <div class="content">
+        <movie-list class="favorites" .class=${"favorite"} .list=${this.favorites}></movie-list>
+        <movie-list class="standard" .class=${"standard"} .list=${this.movies}></movie-list>
+      </div>
     `;
   }
 
